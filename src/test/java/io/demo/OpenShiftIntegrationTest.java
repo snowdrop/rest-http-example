49cc7a67785e99882d2326bdb4d771aabe96ed44
Charles Moulliard <cmoulliard@apache.org>
2016-11-15 17:40:22 +0100

    Add test case for Kubernetes/Openshift. Fix #3

---
+++ src/test/java/io/demo/OpenShiftIntegrationTest.java
@@ -0,0 +1,24 @@
+package io.demo;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static io.fabric8.kubernetes.assertions.Assertions.assertThat;
+
+@RunWith(Arquillian.class)
+@RunAsClient
+public class OpenShiftIntegrationTest {
+
+    @ArquillianResource
+    KubernetesClient client;
+
+    @Test
+    public void testAppProvisionsRunningPods() throws Exception {
+        assertThat(client).deployments().pods().isPodReadyForPeriod();
+    }
+
+}
