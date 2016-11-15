5331d8e3876a00e7e31e56804a8a1458f3abd045
Charles Moulliard <cmoulliard@apache.org>
2016-11-15 12:13:03 +0100

    Add 2 profiles. Use the Red Hat Build Maven Repo. Fix #1

--- src/test/java/io/demo/RestApplicationTest.java
+++ src/test/java/io/demo/RestApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RestApplicationTests {
+public class RestApplicationTest {
 
 	@Test
 	public void contextLoads() {
