2da9ed36ccfdd17a884b17e062a713962f7c4647
Michal Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2017-09-05 13:17:00 +0200

    SB-108: switched from Spring MVC to JAX-RS

--- src/main/java/io/openshift/booster/BoosterApplication.java
+++ src/main/java/io/openshift/booster/BoosterApplication.java
@@ -15,13 +15,20 @@
  */
 package io.openshift.booster;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BoosterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BoosterApplication.class, args);
+    }
+
+    @Bean
+    public JacksonJsonProvider jsonProvider() {
+        return new JacksonJsonProvider();
     }
 }
