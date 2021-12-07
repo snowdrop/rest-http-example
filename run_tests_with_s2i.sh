8f26bd6c3af82d25089c8df16baf7974b2c15de7
Jose <josecarvajalhilario@gmail.com>
2021-12-07 09:27:24 +0100

    Setup apps in s2i script

--- run_tests_with_s2i.sh
+++ run_tests_with_s2i.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+SOURCE_REPOSITORY_URL=${1:-https://github.com/snowdrop/rest-http-example}
+SOURCE_REPOSITORY_REF=${2:-sb-2.4.x}
+
+oc create -f .openshiftio/application.yaml
+oc new-app --template=rest-http -p SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF
+
+sleep 30 # needed in order to bypass the 'Pending' state
+# wait for the app to stand up
+timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"build\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") == "Running" ]]; do sleep 20; done; echo ""'
 
 # Run OpenShift Tests
 ./mvnw -s .github/mvn-settings.xml clean verify -Popenshift,openshift-it -Dunmanaged-test=true
