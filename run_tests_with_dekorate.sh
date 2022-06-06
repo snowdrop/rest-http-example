681d0af97e0a6b30ddd14bca12ec695bbe418cbf
Antonio Costa <antcosta@redhat.com>
2022-06-06 11:06:30 +0200

    Adapt to dekorate

--- run_tests_with_dekorate.sh
+++ run_tests_with_dekorate.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
+SB_VERSION_SWITCH=""
+
+while getopts v: option
+do
+    case "${option}"
+        in
+        v)SB_VERSION_SWITCH="-Dspring-boot.version=${OPTARG}";;
+    esac
+done
+
+echo "SB_VERSION_SWITCH: ${SB_VERSION_SWITCH}"
+
 # Run OpenShift Tests
-./mvnw -s .github/mvn-settings.xml clean verify -Popenshift,openshift-it
+eval "./mvnw -s .github/mvn-settings.xml clean verify -Popenshift,openshift-it ${SB_VERSION_SWITCH}"
