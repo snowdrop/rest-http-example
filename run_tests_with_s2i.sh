665d66d7af05e423e87119b8ce4000109851635a
A.C <antcosta@redhat.com>
2022-11-29 13:10:49 +0100

    feat: add MAVEN_ARGS_APPEND parameter to s2i (#164)

--- run_tests_with_s2i.sh
+++ run_tests_with_s2i.sh
@@ -4,6 +4,7 @@
 #   . --branch-to-test
 #   . --maven-settings
 #   . --maven-mirror-url
+UNMANAGED_PARAMS=""
 SOURCE_REPOSITORY_URL="https://github.com/snowdrop/rest-http-example"
 SOURCE_REPOSITORY_REF="sb-2.7.x"
 MAVEN_SETTINGS_REF=""
@@ -18,6 +19,10 @@ while [ $# -gt 0 ]; do
       --maven-settings) MAVEN_SETTINGS_REF="-s $2";;
       --maven-mirror-url) MAVEN_MIRROR_URL="$2";;
     esac;
+    shift
+  elif [[ $1 == "-D"* ]];
+  then
+    UNMANAGED_PARAMS="${UNMANAGED_PARAMS} $1";
   fi
   shift
 done
@@ -25,11 +30,12 @@ done
 source scripts/waitFor.sh
 
 oc create -f .openshiftio/application.yaml
-oc new-app --template=rest-http -p SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF -p MAVEN_MIRROR_URL=$MAVEN_MIRROR_URL
+oc new-app --template=rest-http -p SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF \
+  -p MAVEN_MIRROR_URL=$MAVEN_MIRROR_URL -p MAVEN_ARGS_APPEND="$UNMANAGED_PARAMS"
 if [[ $(waitFor "rest-http" "app") -eq 1 ]] ; then
   echo "Application failed to deploy. Aborting"
   exit 1
 fi
 
 # Run Tests
-eval "./mvnw ${MAVEN_SETTINGS_REF} clean verify -Popenshift,openshift-it -Dunmanaged-test=true"
+eval "./mvnw ${MAVEN_SETTINGS_REF} clean verify -Popenshift,openshift-it -Dunmanaged-test=true ${UNMANAGED_PARAMS}"
